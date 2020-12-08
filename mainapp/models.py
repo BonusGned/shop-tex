f678ad69f4683edc122da4843123c4b08d65ef45
BonusGned <jesus.moj@mail.ru>
2020-12-08 14:00:46 +0300

    upd

--- mainapp/models.py
+++ mainapp/models.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя категории')
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, blank=True, unique=True)
 
     def __str__(self):
         return self.name
@@ -29,7 +29,7 @@ class Product(models.Model):
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, blank=True, unique=True)
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание', null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
